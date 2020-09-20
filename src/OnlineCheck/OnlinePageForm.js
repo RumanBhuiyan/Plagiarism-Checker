58593244e1291d2ae24337e6792590b9d5b96ed6
CSE <revengerreigns076@gmail.com>
2020-09-20 10:13:52 +0600

    Modifying Midpart of HomePage

--- src/OnlineCheck/OnlinePageForm.js
+++ src/OnlineCheck/OnlinePageForm.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./OnlinePageDesign.css";
+import OnlineMidVector from "../images/plagiarism6.jpg";
 
 function OnlinePageForm() {
   return (
-    <div className="container-fluid">
-      <h2>Online Page Form </h2>
+    <div className="container-fluid overflow-hidden">
+      <div className="row">
+        <div className="col">
+          <img src={OnlineMidVector} alt="mid" width="200" />
+        </div>
+        <div className="col">World</div>
+      </div>
     </div>
   );
 }
