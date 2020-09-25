45123935da2b3af2bca0f9d2d59cdde4325b99c2
CSE <revengerreigns076@gmail.com>
2020-09-25 16:21:59 +0600

    Drawing Scatter plot to show plagiarism

--- Client/src/OfflineCheck/OfflineChart.js
+++ Client/src/OfflineCheck/OfflineChart.js
@@ -1,6 +1,9 @@
-import React from "react";
+/* eslint-disable */
+import React, { useEffect } from "react";
 import { MyContext } from "../index";
 import { useHistory } from "react-router-dom";
+import Chart from "chart.js";
+import randomcolor from "randomcolor";
 
 function OfflineChart() {
   let history = useHistory();
@@ -18,6 +21,51 @@ function OfflineChart() {
   console.log(averagePieData);
   console.log(allFilesPieData);
 
+  useEffect(() => {
+    let graphDatasets = [];
+    for (let i = 0; i < filesNames.length; i++) {
+      let mydata = [];
+      for (let j = 0; j < graphData[i].length; j++) {
+        mydata.push({ x: j + 1, y: graphData[i][j] });
+      }
+      graphDatasets.push({
+        label: filesNames[i],
+        data: mydata,
+        showLine: true,
+        fill: true,
+        backgroundColor: randomcolor(),
+        borderColor: "rgba(0, 200, 0, 1)",
+      });
+    }
+
+    var ctx = document.getElementById("globalChart");
+    var myChart = new Chart(ctx, {
+      type: "scatter",
+      data: {
+        datasets: graphDatasets,
+      },
+      options: {
+        tooltips: {
+          mode: "index",
+          intersect: false,
+        },
+        hover: {
+          mode: "nearest",
+          intersect: true,
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+  }, []);
+
   return (
     <div>
       <div className="container-fluid bg-dark offlineNavDiv">
@@ -25,6 +73,7 @@ function OfflineChart() {
         <button onClick={() => history.push("/")}>Home</button>
         <button onClick={() => history.push("/online")}>OnlineCheck</button>
       </div>
+      <canvas id="globalChart"></canvas>
     </div>
   );
 }
