d6b27aceca9901b38f54eefdc1dbf7aa8f108319
CSE <revengerreigns076@gmail.com>
2020-09-29 16:51:31 +0600

    Successfully Scraped Google results in my console

--- Server/server.js
+++ Server/server.js
@@ -59,80 +59,82 @@ app.post("/diffwords", (req, res) => {
   res.send(copiedpart);
 });
 
-app.post("/searchonline", (req, response) => {
-  let searchItem = `https://www.google.com/search?q=${req.body.text}`;
-  let sentItems = [];
-  let keepLinks = [];
+app.get("/googleresponse", (req, res) => {
+  let searchingText = `https://www.google.com/search?q=${req.query.text}`;
 
-  searchEngine(searchItem)
+  searchEngine(searchingText)
     .then((res) => {
-      let $ = cheerio.load(res.body);
-      let allAnchorTags = $("a");
-      let extractedLinks = [];
-
-      for (let i = 0; i < allAnchorTags.length; i++) {
-        let keep = queryString.parse(allAnchorTags[i].attribs.href);
-        let link = Object.values(keep)[0];
-        if (link !== null && link.includes("https")) {
-          extractedLinks.push(link);
-        }
-      }
-      //Removing Same Links
-      let finalLinks = extractedLinks.filter((item, index) => {
-        return extractedLinks.indexOf(item) === index;
-      });
-      // Assigning Final Links to keepLinks[]
-      setTimeout(() => {
-        keepLinks = finalLinks;
-      }, 1000);
+      fs.writeFileSync("./response.txt", res.body);
     })
     .catch((error) => {
       console.log(error);
     });
+  res.send("Response Wrote in a Text file");
+});
 
-  setTimeout(() => {
-    for (let i = 0; i < keepLinks.length; i++) {
-      try {
-        if (!isImageUrl(keepLinks[i])) {
-          setTimeout(() => {
-            textract.fromUrl(keepLinks[i], (error, text) => {
-              if (error) {
-                console.log(error);
-              } else {
-                if (text.length !== 0 && text.includes(req.body.text)) {
-                  let object = {
-                    content: req.body.text,
-                    link: keepLinks[i],
-                    copied: queryText,
-                  };
-                  sentItems.push(object);
-                  console.log(object);
-                } else {
-                  let diffResults = jsdiff.diffWords(req.body.text, text);
-                  let copiedpart = "";
-                  for (let i = 0; i < diffResults.length; i++) {
-                    if (Object.keys(diffResults[i]).length === 2) {
-                      copiedpart += diffResults[i].value;
-                    }
-                  }
-                  let object = {
-                    content: req.body.text,
-                    link: keepLinks[i],
-                    copied: copiedpart,
-                  };
-                  sentItems.push(object);
-                  console.log(object);
-                }
-              }
-            });
-          }, 3000);
-        }
-      } catch (error) {
-        console.log(error);
-      }
+app.post("/searchonline", (req, res) => {
+  let response = fs.readFileSync("./response.txt", "utf-8");
+
+  let $ = cheerio.load(response);
+  let allAnchorTags = $("a");
+  let extractedLinks = [];
+
+  for (let i = 0; i < allAnchorTags.length; i++) {
+    let keep = queryString.parse(allAnchorTags[i].attribs.href);
+    let link = Object.values(keep)[0];
+    if (link !== null && link.includes("https")) {
+      extractedLinks.push(link);
     }
-  }, 5000);
-  response.send("Running your request......");
+  }
+
+  // Removing same links
+  let finalLinks = extractedLinks.filter((item, index) => {
+    return extractedLinks.indexOf(item) === index;
+  });
+
+  let sentItems = [];
+
+  for (let i = 0; i < finalLinks.length; i++) {
+    try {
+      if (!isImageUrl(finalLinks[i])) {
+        setTimeout(() => {
+          textract.fromUrl(finalLinks[i], (error, text) => {
+            if (error) {
+              console.log(error);
+            } else {
+              if (text.length !== 0 && text.includes(req.body.text)) {
+                let object = {
+                  content: req.body.text,
+                  link: finalLinks[i],
+                  copied: req.body.text,
+                };
+                sentItems.push(object);
+                console.log(object);
+              } else {
+                let diffResults = jsdiff.diffWords(req.body.text, text);
+                let copiedpart = "";
+                for (let i = 0; i < diffResults.length; i++) {
+                  if (Object.keys(diffResults[i]).length === 2) {
+                    copiedpart += diffResults[i].value;
+                  }
+                }
+                let object = {
+                  content: req.body.text,
+                  link: finalLinks[i],
+                  copied: copiedpart,
+                };
+                sentItems.push(object);
+                console.log(object);
+              }
+            }
+          });
+        }, 3000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  res.send("See in the console");
 });
 
 app.listen(3001, () => {
